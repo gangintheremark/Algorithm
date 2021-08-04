13c5fcbed3b684e60eae619118f8fb2d32a12618
gangintheremark <nkh95@naver.com>
2021-08-04 21:30:34 +0900

    백준 10828 수정

--- 210801/백준10828.c
+++ 210801/백준10828.c
@@ -1,55 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+#define INIT_CAPACITY 10000
 
-struct node {
-	int data;
-	struct node* next;
-};
 struct stack_type {
-	struct node* top;
+	int* contents;
+	int top;
+	int size;
 };
 typedef struct stack_type Stack;
-typedef struct node Node;
-int count = 0;
+Stack* create();
+void push(Stack*);
+int pop(Stack*);
+int top(Stack*);
 
 bool is_empty(Stack* s)
 {
-	return s->top == NULL;
+	return s->top == -1;
 }
-void push(Stack* s)
-{
-	int index;
-	scanf("%d", &index);
 
-	Node* tmp = (Node*)malloc(sizeof(Node));
-	tmp->data = index;
-	tmp->next = s->top;
-	s->top = tmp;
-	count++;
-}
-int pop(Stack* s)
-{
-	if (s->top ==NULL)
-		return -1;
-
-	int index = s->top->data;
-	s->top = s->top->next;
-	count--;
-	return index;
-}
-int top(Stack* s)
-{
-	if (is_empty(&s))
-		return -1;
-	else
-		return s->top->data;
-}
 int main()
 {
-	int index; 
+	int index;
 	char buffer[10000];
-	Stack* s = NULL;
+	Stack* s = create();
 
 	scanf("%d", &index);
 
@@ -57,18 +32,58 @@ int main()
 		scanf("%s", buffer);
 
 		if (strcmp(buffer, "push") == 0)
-			push(&s);
+			push(s);
 		else if (strcmp(buffer, "pop") == 0)
-			printf("%d", pop(&s));
+			printf("%d\n", pop(s));
 		else if (strcmp(buffer, "empty") == 0)
-			printf("%d", is_empty(&s));
+			printf("%d\n", is_empty(s));
 		else if (strcmp(buffer, "size") == 0)
-			printf("%d", count);
+			printf("%d\n", s->size);
 		else if (strcmp(buffer, "top") == 0)
-			printf("%d", top(&s));
+			printf("%d\n", top(s));
 		else
 			break;
 	}
+	
 	return 0;
 }
+Stack* create()
+{
+	Stack* s = malloc(sizeof(struct stack_type));
 
+	s->contents = (int*)malloc(INIT_CAPACITY * sizeof(int));
+
+	if (s->contents == NULL) {
+		free(s);
+	}
+
+	s->top = -1;
+	s->size = 0;
+	return s;
+}
+void push(Stack* s)
+{
+	int index;
+	scanf("%d", &index);
+
+	s->top++;
+	s->contents[s->top] = index;
+	s->size++;
+}
+
+int pop(Stack* s)
+{
+	if (is_empty(s))
+		return -1;
+	s->top--;
+	s->size--;
+	return s->contents[s->top + 1];
+}
+
+int top(Stack* s)
+{
+	if (is_empty(s))
+		return -1;
+	else
+		return s->contents[s->top];
+}
