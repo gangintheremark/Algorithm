920dbddfa34dbf0898dbfff1d850771549e980f9
gangintheremark <nkh9587@naver.com>
2021-11-09 00:59:06 +0900

    동적계획법 문제

---
+++ 백준 문제풀이/동적계획법/백준 1003.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int T = s.nextInt();
+        int[] data = new int[41];
+        data[0] = 0;
+        data[1] = data[2] = 1;
+        for (int i = 3; i < 41; i++)
+            data[i] = data[i - 1] + data[i - 2];
+
+        for (int i = 0; i < T; i++) {
+            int N = s.nextInt();
+
+            if (N == 0) {
+                System.out.println("1 0");
+            } else if (N == 1) {
+                System.out.println("0 1");
+            } else
+                System.out.println(data[N - 1] + " " + data[N]);
+        }
+    }
+}
